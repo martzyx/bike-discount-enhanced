b1ddba3ce73a10ef768e9aeb7c22fe5be42ed8ce
martz <mkg@clerk.io>
2025-03-10 17:10:00 +0100

    first test

---
+++ change-dk.js
@@ -0,0 +1,9 @@
+    const selectedElement = document.querySelector('.delivery .service--entry.selected');
+    if (selectedElement) {
+        const deliveryValue = selectedElement.getAttribute('data-delivery');
+        if (deliveryValue !== '7') {
+            const deliveryID = "7";
+            document.querySelector(".hidden_delivery_input").value = deliveryID;
+            document.querySelector(".delivery--form.desktop").submit();
+        }
+    }

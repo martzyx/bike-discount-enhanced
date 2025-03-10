a95318569ccbf1b95f234ede56a49fb3e312b8cf
martz <mkg@clerk.io>
2025-03-10 23:03:03 +0100

    prettier vim

--- ensure-dk.js
+++ ensure-dk.js
@@ -1,11 +1,13 @@
 if (window.location.pathname != "/en/account/logout") {
-    const selectedElement = document.querySelector('.delivery .service--entry.selected');
-    if (selectedElement) {
-        const deliveryValue = selectedElement.getAttribute('data-delivery');
-        if (deliveryValue !== '7') {
-            const deliveryID = "7";
-            document.querySelector(".hidden_delivery_input").value = deliveryID;
-            document.querySelector(".delivery--form.desktop").submit();
-        }
+  const selectedElement = document.querySelector(
+    ".delivery .service--entry.selected",
+  );
+  if (selectedElement) {
+    const deliveryValue = selectedElement.getAttribute("data-delivery");
+    if (deliveryValue !== "7") {
+      const deliveryID = "7";
+      document.querySelector(".hidden_delivery_input").value = deliveryID;
+      document.querySelector(".delivery--form.desktop").submit();
     }
+  }
 }

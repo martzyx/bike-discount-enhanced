09314eee09355f3905ff90c431284d5aa15da71c
martz <mkg@clerk.io>
2025-03-10 22:17:20 +0100

    start login feat, add todo

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('bike-discount enhanced extension installed');
 });
+
+const username = 'your_username';
+const password = 'your_secure_password';
+
+chrome.storage.local.set({ username: username, password: password }, function() {
+    console.log('Username and password is set');
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+todo:
+- make user able to add login deets using background.js
+- retrieve deets in login.js
+- redirect user to login page
+- submit form with provided deets
+- encryption of deets

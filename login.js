19d71087f435e7810a2a1d884eb61dd842ff2ab5
martz <mkg@clerk.io>
2025-03-11 18:38:55 +0100

    first login draft, need to handle wrong creds

--- login.js
+++ login.js
@@ -1,12 +1,41 @@
-chrome.storage.sync.get(['username', 'password'], function(result) {
-  if (result.username && result.password) {
-//  const usernameField = document.querySelector('input[name="username"]');
-//  const passwordField = document.querySelector('input[name="password"]');
+const loginStatusEl = document.querySelector(".account--link");
+if (
+  loginStatusEl &&
+  !loginStatusEl.classList.contains("account--user-loggedin")
+) {
+  //  if (window.dataLayer[0].visitorLoginState == "Logged Out") {
+  chrome.storage.sync.get(["username", "password"], function (result) {
+    if (result.username && result.password) {
+      const csrfToken = getCookieValue("__csrf_token-1");
+      const formData = new FormData();
+      formData.append("sTarget", "account");
+      formData.append("email", result.username);
+      formData.append("password", result.password);
+      formData.append("__csrf_token", csrfToken);
 
-//    if (usernameField && passwordField) {
-//      usernameField.value = result.username;
-//      passwordField.value = result.password;
-//    }
-    console.log(result.username, result.password)
-  }
-});
+      fetch(
+        "https://www.bike-discount.de/en/account/login/sTarget/account/sTargetAction/index",
+        {
+          method: "POST",
+          body: formData,
+        }
+      )
+        .then((data) => {
+          if (typeof data === "string") {
+            console.error("Login failed:", data);
+          } else {
+            console.log("Success:", data);
+            //          location.reload();
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+      function getCookieValue(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(";").shift();
+      }
+    }
+  });
+}
